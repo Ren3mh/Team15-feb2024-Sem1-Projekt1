279a69473fae45e2f779c578d902a854e782754f
René Mose Hansen <rmha63250@edu.ucl.dk>
2024-02-15 10:17:17 +0100

    Opdatering af opg. filer

---
+++ Rektangel1/Program - Gruppe2.cs
@@ -0,0 +1,15 @@
+﻿namespace Rektangel1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal højde = 500m;
+
+            decimal bredde;
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Rektangel1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Rektangel1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int HøjdeRekt;
+
+            Console.WriteLine("Hej, skriv din højde på din rektangel");
+            HøjdeRekt = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine(HøjdeRekt);
+            Console.ReadLine();
+        }
+    }
+}
